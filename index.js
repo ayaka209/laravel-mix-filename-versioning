4b0caa5f61919aa968fed771aa1d8617279ba0aa
ayaka209 <weloveayaka@gmail.com>
2019-04-11 14:07:17 +0800

    Update index.js
    work with other plugin

--- index.js
+++ index.js
@@ -2,40 +2,41 @@ const fs = require('fs')
 const path = require('path');
 
 class LaravelMixFilenameVersioning {
-  apply (compiler) {
-    compiler.plugin('done', function(stats) {
-      const newAssets = {};
+    apply (compiler) {
+        compiler.plugin('after-emit', function(compilation,callback) {
+            const newAssets = {};
 
-      Object.keys(stats.compilation.assets).forEach(assetName => {
-        let originalAssetNameParts = path.parse(assetName);
-        let newAssetFile = new File(path.join(Config.publicPath, assetName));
-        let newAssetFileName = newAssetFile.segments.name + '.' + newAssetFile.version().substr(0, 8) + newAssetFile.segments.ext;
+            Object.keys(compilation.assets).forEach(assetName => {
+                let originalAssetNameParts = path.parse(assetName);
+                let newAssetFile = new File(path.join(Config.publicPath, assetName));
+                let newAssetFileName = newAssetFile.segments.name + '.' + newAssetFile.version().substr(0, 8) + newAssetFile.segments.ext;
 
-        newAssetFile.rename(newAssetFileName);
+                newAssetFile.rename(newAssetFileName);
 
-        let newAssetKeyName = path.join(originalAssetNameParts.dir, newAssetFileName);
-        let newAssetFullFileName = path.join(newAssetFile.segments.base, newAssetFileName);
-        newAssets[newAssetKeyName] = stats.compilation.assets[assetName];
+                let newAssetKeyName = path.join(originalAssetNameParts.dir, newAssetFileName);
+                let newAssetFullFileName = path.join(newAssetFile.segments.base, newAssetFileName);
+                newAssets[newAssetKeyName] = compilation.assets[assetName];
 
-        if (newAssets[newAssetKeyName].hasOwnProperty('existsAt')) {
-          newAssets[newAssetKeyName].existsAt = newAssetFullFileName;
-        }
-        if (newAssets[newAssetKeyName].hasOwnProperty('absolutePath')) {
-          newAssets[newAssetKeyName].absolutePath = newAssetFullFileName;
-        }
+                if (newAssets[newAssetKeyName].hasOwnProperty('existsAt')) {
+                    newAssets[newAssetKeyName].existsAt = newAssetFullFileName;
+                }
+                if (newAssets[newAssetKeyName].hasOwnProperty('absolutePath')) {
+                    newAssets[newAssetKeyName].absolutePath = newAssetFullFileName;
+                }
 
-        // this is only a fix for the incorrect asset binding in CustomTaskPlugins.js
-        newAssets[newAssetKeyName].size = function (assetAbsolutePath) {
-          return new File(assetAbsolutePath).size();
-        }.bind(null, newAssetFullFileName);
+                // this is only a fix for the incorrect asset binding in CustomTaskPlugins.js
+                newAssets[newAssetKeyName].size = function (assetAbsolutePath) {
+                    return new File(assetAbsolutePath).size();
+                }.bind(null, newAssetFullFileName);
 
-        Mix.manifest.manifest[assetName] = newAssetKeyName;
-      });
+                Mix.manifest.manifest[assetName] = newAssetKeyName;
+            });
 
-      Mix.manifest.refresh();
-      stats.compilation.assets = newAssets;
-    });
-  }
+            Mix.manifest.refresh();
+            compilation.assets = newAssets;
+            callback();
+        });
+    }
 }
 
 module.exports = LaravelMixFilenameVersioning;
